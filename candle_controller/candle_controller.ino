ce40737bc1699f1a413023363cc3185e9844c453
picode98 <saaman1377@gmail.com>
2024-08-12 22:13:07 -0400

    Add basic OTA update mechanism (adapted from ArduinoOTA/OTAWebUpdater example)

--- candle_controller/candle_controller.ino
+++ candle_controller/candle_controller.ino
@@ -15,6 +15,8 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <WebServer.h>
+#include <Update.h>
+#include <Esp.h>
 
 WiFiMulti WiFiMulti;
 #include <esp_mac.h>
@@ -346,6 +348,30 @@ void setup()
         
         Serial.printf("Palette of %d colors applied.\n", NUM_PALETTE_COLORS);
         server.send(200, "text/plain", "");
+    });
+    server.on("/update_sketch", HTTP_POST, []() {
+      server.sendHeader("Connection", "close");
+      server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+      ESP.restart();
+    }, []() {
+      HTTPUpload& upload = server.upload();
+      if (upload.status == UPLOAD_FILE_START) {
+        Serial.printf("Update: %s\n", upload.filename.c_str());
+        if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+          Update.printError(Serial);
+        }
+      } else if (upload.status == UPLOAD_FILE_WRITE) {
+        /* flashing firmware to ESP*/
+        if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+          Update.printError(Serial);
+        }
+      } else if (upload.status == UPLOAD_FILE_END) {
+        if (Update.end(true)) { //true to set the size to the current progress
+          Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+        } else {
+          Update.printError(Serial);
+        }
+      }
     });
     server.begin();
 
